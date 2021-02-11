a0e281328e7ba6d9db8608f807ce8d8e75be322f
LabMikey <78513560+LabMikey@users.noreply.github.com>
2021-02-12 11:58:23 +1300

    Create 00_MMF_base_v1.py

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,32 @@
+# import statements
+
+
+# functions go here
+
+
+# *************  Main Routine *************
+
+# Set up dictionaries / Lists needed to hold data
+
+# Ask user if they have used the progra before & and show instructions of necessary
+
+    # Loop to get ticket details
+
+    # Get ticket holder name and check its not blank
+
+    # Get ticket holder age and check it is an integer that is more than or equal to 12 and less than 130
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # ask for payment method (and apply surcharge of necessary)
+
+
+
+# Calculate Total sales and profit
+
+# Output data to text file
+
