b38b96229773bab4deb582374942ceae6b57dd21
LabMikey <78513560+LabMikey@users.noreply.github.com>
2021-02-19 11:52:32 +1300

    Working ticket loop

---
+++ 02_ticket_loop_v1.py
@@ -0,0 +1,12 @@
+# start of loop
+
+# initialise loop so that it run at leat once
+name = ""
+count= 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+
+    # Get details...
+    name = input("Name: ")
+    count += 1
