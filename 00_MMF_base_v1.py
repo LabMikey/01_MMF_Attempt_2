6213c84ab5e4504f8d58b40cbf6134369ac3172f
LabMikey <78513560+LabMikey@users.noreply.github.com>
2021-02-18 13:05:14 +1300

    working not blank

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -3,18 +3,32 @@
 
 # functions go here
 
+# checks that ticket name is not blank
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        # If name is not blank, program continues
+        if response != "":
+            return response
+        else:
+            print("Sorry - this can't be blank")
+
 
 # *************  Main Routine *************
 
 # Set up dictionaries / Lists needed to hold data
 
-# Ask user if they have used the progra before & and show instructions of necessary
+# Ask user if they have used the program before & and show instructions of necessary
 
-    # Loop to get ticket details
+# Loop to get ticket details
 
     # Get ticket holder name and check its not blank
+        name = not_blank("Name: ")
 
-    # Get ticket holder age and check it is an integer that is more than or equal to 12 and less than 130
+    # Get age (between 12 and 130)
 
     # Calculate ticket price
 
