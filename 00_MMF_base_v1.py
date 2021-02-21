5169470e5fa4c14ab0433563153998ec5a30e04b
LabMikey <78513560+LabMikey@users.noreply.github.com>
2021-02-22 10:20:25 +1300

    Working ticket loop v3

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -24,8 +24,40 @@ def not_blank(question):
 # Ask user if they have used the program before & and show instructions of necessary
 
 # Loop to get ticket details
+        # start of loop
 
-    # Get ticket holder name and check its not blank
+        # initialise loop so that it runs at least once
+        name = ""
+        count = 0
+        MAX_TICKETS = 5
+
+        while name != "xxx" and count < MAX_TICKETS:
+
+            # tells user how many seats are left
+            if count < 4:
+                print("you have {} seats "
+                      "left".format(MAX_TICKETS - count))
+
+            # Warns user that only one seat is left!
+            else:
+                print("*** There is ONE seat left!! ***")
+
+            # Get details...
+
+            # Get name (can't be blank)
+            name = input("name:")
+            count += 1
+            print()
+
+        if count == MAX_TICKETS:
+            print("You have sold all the available tickets!")
+        else:
+            print("You have sold {} tickets.  \n"
+                  "There are {} places still available"
+                  .format(count, MAX_TICKETS - count))
+
+
+            # Get ticket holder name and check its not blank
         name = not_blank("Name: ")
 
     # Get age (between 12 and 130)
