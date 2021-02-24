89a6aa92e61e28e16584693e195dd04874337d9c
LabMikey <78513560+LabMikey@users.noreply.github.com>
2021-02-24 14:28:18 +1300

    00_MMF_base_v1 working

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -16,6 +16,28 @@ def not_blank(question):
         else:
             print("Sorry - this can't be blank")
 
+# checks for an interger between two values
+def int_check(question, low_num, high_num):
+
+    error = "Please enter a whole number between {} " \
+    "and {}".format(low_num, high_num)
+
+    valid = False
+    while not valid:
+
+        # ask user for number and check it is valid
+        try:
+            response = int(input(question))
+
+            if low_num < response < high_num:
+                return response
+
+            else:
+                print(error)
+
+        # If an interger is not entered, display an error
+        except ValueError:
+            print(error)
 
 # *************  Main Routine *************
 
@@ -26,39 +48,46 @@ def not_blank(question):
 # Loop to get ticket details
         # start of loop
 
-        # initialise loop so that it runs at least once
-        name = ""
-        count = 0
-        MAX_TICKETS = 5
+# initialise loop so that it runs at least once
+name = ""
+count = 0
+MAX_TICKETS = 5
 
-        while name != "xxx" and count < MAX_TICKETS:
+while name != "xxx" and count < MAX_TICKETS:
 
-            # tells user how many seats are left
-            if count < 4:
-                print("you have {} seats "
-                      "left".format(MAX_TICKETS - count))
+    # tells user how many seats are left
+    if count < 4:
+        print("you have {} seats "
+              "left".format(MAX_TICKETS - count))
 
-            # Warns user that only one seat is left!
-            else:
-                print("*** There is ONE seat left!! ***")
+    # Warns user that only one seat is left!
+    else:
+        print("*** There is ONE seat left!! ***")
 
-            # Get details...
+    # Get details...
 
-            # Get name (can't be blank)
-            name = input("name:")
-            count += 1
-            print()
+    # Get name (can't be blank)
+    name = not_blank("Name:")
 
-        if count == MAX_TICKETS:
-            print("You have sold all the available tickets!")
-        else:
-            print("You have sold {} tickets.  \n"
-                  "There are {} places still available"
-                  .format(count, MAX_TICKETS - count))
+    # end loop if the exit code is entered
+    if name == "xxx":
+        break
+
+    count += 1
+
+    # main routine goes here
+    age = int_check("Age: ", 12, 130)
+
+if count == MAX_TICKETS:
+    print("You have sold all the available tickets!")
+else:
+    print("You have sold {} tickets.  \n"
+          "There are {} places still available"
+          .format(count, MAX_TICKETS - count))
 
 
             # Get ticket holder name and check its not blank
-        name = not_blank("Name: ")
+
 
     # Get age (between 12 and 130)
 
