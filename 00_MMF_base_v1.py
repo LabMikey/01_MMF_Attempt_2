5cf76b9ab298a13389de71416a50ea3e30b321d8
Michael Labaclado <labacladom70419@masseyhigh.school.nz>
2021-03-08 20:29:59 +0000

    MMF base with ticket price

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -1,7 +1,7 @@
 # import statements
 
-
 # functions go here
+
 
 # checks that ticket name is not blank
 def not_blank(question):
@@ -16,7 +16,9 @@ def not_blank(question):
         else:
             print("Sorry - this can't be blank")
 
+
 # checks for an interger between two values
+
 
 # checks for an interger more than 0
 def int_check(question):
@@ -37,6 +39,7 @@ def int_check(question):
         except ValueError:
             print(error)
 
+
 # *************  Main Routine *************
 
 # Set up dictionaries / Lists needed to hold data
@@ -44,19 +47,21 @@ def int_check(question):
 # Ask user if they have used the program before & and show instructions of necessary
 
 # Loop to get ticket details
-        # start of loop
+# start of loop
 
 # initialise loop so that it runs at least once
-name = ""
-count = 0
 MAX_TICKETS = 5
 
-while name != "xxx" and count < MAX_TICKETS:
+name = ""
+ticket_count = 0
+ticket_sales = 0
+
+
+while name != "xxx" and ticket_count < MAX_TICKETS:
 
     # tells user how many seats are left
-    if count < 4:
-        print("you have {} seats "
-              "left".format(MAX_TICKETS - count))
+    if ticket_count < MAX_TICKETS - 1:
+        print("you have {} seats " "left".format(MAX_TICKETS - ticket_count))
 
     # Warns user that only one seat is left!
     else:
@@ -82,17 +87,30 @@ while name != "xxx" and count < MAX_TICKETS:
         print("That is very old - it looks like a mistake")
         continue
 
-    count += 1
+    if age < 16:
+        ticket_price = 7.5
+    elif age < 65:
+        ticket_price = 10.5
+    else:
+        ticket_price = 6.5
+    
+    ticket_count += 1
+    ticket_sales += ticket_price
 
-if count == MAX_TICKETS:
+# End of ticket loop
+# calculate ticket profit
+ticket_profit = ticket_sales - (5 * ticket_count)
+print("Ticket profit: ${:.2f}".format(ticket_profit))
+
+# Tell user that if they have unsold tickets
+if ticket_count == MAX_TICKETS:
     print("You have sold all the available tickets!")
 else:
     print("You have sold {} tickets.  \n"
-          "There are {} places still available"
-          .format(count, MAX_TICKETS - count))
+          "There are {} places still available".format(ticket_count,
+                                                       MAX_TICKETS - ticket_count))
 
-            # Get ticket holder name and check its not blank
-
+    # Get ticket holder name and check its not blank
 
     # Get age (between 12 and 130)
 
@@ -104,9 +122,6 @@ else:
 
     # ask for payment method (and apply surcharge of necessary)
 
-
-
 # Calculate Total sales and profit
 
 # Output data to text file
-
