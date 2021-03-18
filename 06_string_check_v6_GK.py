6d6ca3007f0473985de3d3a59fd0c29c0f1fb837
Michael Labaclado <labacladom70419@masseyhigh.school.nz>
2021-03-18 00:19:19 +0000

    partial string check v5/6

--- 06_string_check_v6_GK.py
+++ 06_string_check_v6_GK.py
@@ -6,24 +6,24 @@ def string_check(choice, options):
 
   for var_list in options:
 
-    # if the snack is one of the lists, return the filter
+    # if the snack is in one of the lists, return the full 
     if choice in var_list:
 
-      # get full name of snack and put it
+      # Get full name of snack and put it
       # in title case so it looks nice when outputted
       chosen = var_list[0].title()
       is_valid = "Yes"
-      break      
+      break
 
-  # if the chosen option is not valid, set is_valid to no
-    else:
-      is_valid = "no"
+    # If the chosen is not valid, set is_valid to no
+  else:
+    is_valid = "no"
 
-# if the snack is not OK - ask question again.
-    if is_valid == "yes":
-      return chosen
-    else:
-      return "invalid choice"
+  # if the snack is not OK - ask question again
+  if is_valid == "yes":
+    return chosen
+  else:
+    return "invalid choice"
 
 
 # regular expression to find if item starts with a number
