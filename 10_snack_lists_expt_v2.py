c48a519cb666784f81f1fcccd4379f53a72ad0c1
Michael Labaclado <labacladom70419@masseyhigh.school.nz>
2021-03-28 21:14:51 +0000

    working snack list expt

--- 10_snack_lists_expt_v2.py
+++ 10_snack_lists_expt_v2.py
@@ -13,15 +13,16 @@ snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
 snack_menu_dict = {
   'Popcorn': popcorn,
   'Water': water,
-  'Pita chips': pita_chips,
+  'Pita Chips': pita_chips,
   'M&Ms': mms,
   'Orange Juice': orange_juice
 }
 
 test_data = [
   [[2, 'Popcorn'], [1, 'Pita Chips'], [1, 'Orange Juice']],
-  [[1, 'Pita Chips'], [1, 'Orange Juice'], [2, 'M&Ms']],
-  [[1, 'Water']]
+  [[1, 'Popcorn'], [1, 'Orange Juice'], [2, 'M&Ms']],
+  [[1, 'Water'], [1, 'Orange Juice']],
+  [[2, 'Popcorn'], [1, 'Water']]
 ]
 
 count = 0
@@ -44,4 +45,9 @@ for client_order in test_data:
       add_list = snack_menu_dict[to_find]
       add_list[-1] = amount
 
-print(snack_lists)
+print()
+print("Popcorn: ", snack_lists[0])
+print("M&Ms:", snack_lists[1])
+print("Pita Chips:", snack_lists[2])
+print("Water:", snack_lists[3])
+print("Orange Juice", snack_lists[4])
